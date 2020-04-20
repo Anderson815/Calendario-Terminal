b6a0df5e7cde358692721119f8fdbce03ba282c1
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2020-04-20 07:35:48 -0300

    versão 1

--- src/pkg03/calendario/Cal.java
+++ src/pkg03/calendario/Cal.java
@@ -10,163 +10,51 @@ public class Cal {
     
     private int mes;
     private int ano;
-    
-    //Método InterfaceGrafica
-    public void exibirMes(){
-        this.interfaceCabecalhoMesAno(this.getMes(), this.getAno());
-        this.interfaceCabecalhoSemana();
-        this.interfaceCalendario();
-    }
-        
-    // Métodos Auxiliares Nível 1
-    private void interfaceCabecalhoMesAno(int m, int a){
-        String meses[] = {"JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
-                        "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"};        
-        int margem = (34 - (meses[m].length() + 8))/2;
-                
-        for(int contadorLinha = 0; contadorLinha <= 3; contadorLinha++){
-            for(int contadorColuna = 0; contadorColuna <= 35; contadorColuna++){
-                if((contadorLinha == 0 || contadorLinha == 3) && (contadorColuna != 0 && contadorColuna != 35)){
-                    System.out.print("_");
-                }else if(contadorLinha != 0 && (contadorColuna == 0 || contadorColuna == 35)){
-                    System.out.print("|");
-                }
-                else{
-                    if(contadorLinha == 2 && contadorColuna == margem + 1){
-                        System.out.print(meses[m] + " de " + a);
-                        contadorColuna += meses[m].length() + 7;
-                    }else{
-                        System.out.print(" ");
-                    }
-                }
-            }
-            System.out.print("\n");
-        }       
-    }
-    
-    // Métodos Auxiliares Nível 1
-    private void interfaceCabecalhoSemana(){
-        String semana[] = {"DO", "SE", "TE", "QA", "QI", "SE", "SA"};
-        int contadorColuna = 0;
-        int indice = 0;
-        
-        do{
-            if(contadorColuna % 5 == 2){
-                System.out.print(semana[indice]);
-                contadorColuna++;
-                indice++;
-            }else if(contadorColuna % 5 == 0){
-                System.out.print("|");
-            }else{
-                System.out.print(" ");
-            }
-            contadorColuna++; 
-        }while(contadorColuna <= 35);
-        System.out.println("");
-    }
-    
-    // Métodos Auxiliares Nível 1
-    private void interfaceCalendario(){
-        
-        int dia = 1;
-        int semanas_do_mes = this.semanas_do_mes();
-        boolean intervalo;
-        
-        
-        for(int contadorLinha = 0; contadorLinha <= semanas_do_mes * 3 ; contadorLinha++){
-            for(int contadorColuna = 0; contadorColuna <= 35; contadorColuna++){
-                if(contadorLinha % 3 == 0 && contadorColuna % 5 != 0){
-                    System.out.print("_");
-                }else if(contadorColuna % 5 == 0){
-                    System.out.print("|");
-                }else{
-                    intervalo = ((contadorLinha == 2 && contadorColuna >= (this.getS() * 5) - 3)
-                                    || (contadorLinha > 2 && dia <= 28 + this.getRm()[this.getMes()]));
-                    if(intervalo && contadorLinha % 3 == 2 && contadorColuna % 5 == 2){
-                        String d = (dia < 10)? "0" + Integer.toString(dia): Integer.toString(dia);
-                        System.out.print(d);
-                        contadorColuna++;
-                        dia++;
-                    }else{
-                        System.out.print(" ");
-                    }
-                }
-            }
-            System.out.print("\n");
-        }    
-    }
-    
-    //Método Auxiliar nível 2
-    private int semanas_do_mes(){
-        int semanas_mes = 5;
-        
-        if(this.getS() == 1 && this.getRm()[this.getMes()] == 0){
-            semanas_mes = 4;
-        }else if((this.getS() == 6 || this.getS() == 7) && this.getRm()[this.getMes()] == 3){
-            semanas_mes = 6;
-        }else if(this.getS() == 7 && this.getRm()[this.getMes()] == 2){
-            semanas_mes = 6;
-        }else{
-            semanas_mes = 5;
-        }
-        
-        return semanas_mes;
-    }
-    
-    //Método InterfaceGráfica
-    public void opcoes(){
-        Scanner leitor  = new Scanner(System.in);
-        String op;
-        
-        System.out.println("\n\n");
-        System.out.println("|--- LISTA DE AÇÕES ---|");
-        System.out.println("| > - Avançar um mês   |");
-        System.out.println("| < - Voltar um mês    |");
-        System.out.println("| p - Pesquisar um mês |");
-        System.out.println("| s - Sair             |");
-        System.out.println("|______________________|");
-        System.out.print("O que você deseja fazer?: ");
-        op = leitor.next();
-        
-        this.opcao(op);
-    }
-    
-    // Métodos Auxiliares Nível 1
-    private void opcao(String op){
-        switch(op){
-            case ">":
-                this.avancarMes();
-                break;
-            case "<":
-                break;
-            case "p":
-                break;
-            case "s":
-                break;
-            default:
-                System.out.println("!!! OPÇÃO INVÁLIDA !!!");
-        }
-    }
-    
+            
     //Método principal
     public void avancarMes(){
-        this.setMes(this.getMes() + 1);
+        int anoReferencia = this.getAno();
+        int mesReferencia = this.getMes();
+        int diaReferencia = this.getS();
+        
+        if(this.getMes() < 11)this.setMes(this.getMes() + 1);
+        else{
+            this.setMes(0);
+            this.setAno(this.getAno() + 1);
+        }
+        this.atualizarS(anoReferencia, mesReferencia, diaReferencia);
     } 
     
     //Método principal
     public void voltarMes(){
-        this.setMes(this.getMes() - 1);
+        int anoReferencia = this.getAno();
+        int mesReferencia = this.getMes();
+        int diaReferencia = this.getS();
+        
+        if(this.getMes() > 0)this.setMes(this.getMes() - 1);
+        else{ 
+            this.setMes(11);
+            this.setAno(this.getAno() -1);
+        }
+        
+        this.atualizarS(anoReferencia, mesReferencia, diaReferencia);
     }
     
     //Método principal
     public void pesquisarMes(int m, int a){
+        int anoReferenica = this.getAno();
+        int mesReferencia = this.getMes();
+        int diaReferencia = this.getS();
+        
         this.setAno(a);
         this.setMes(m);
+        
+        this.atualizarS(anoReferenica, mesReferencia, diaReferencia);
     }
     
     //Método Auxiliar
     private void atualizarS(int anoReferencia, int mesReferencia, int diaSReferencia){        
-        int dsa = diaSReferencia; //dia da semana artual
+        int dsa = diaSReferencia; //dia da semana atual
         int comecoMes = mesReferencia;
         
         if((this.getAno() == anoReferencia && this.getMes() > mesReferencia) || this.getAno() > anoReferencia){
@@ -187,9 +75,13 @@ public class Cal {
             this.setS(dsa);
             
         }else if((this.getAno() == anoReferencia && this.getMes() < mesReferencia) || this.getAno() < anoReferencia){
+
             int rm = 0;
             if(comecoMes > 0)comecoMes = comecoMes - 1;
-            else comecoMes = 11;
+            else{ 
+                comecoMes = 11;
+                anoReferencia--;
+            }
             
             for(int ano = anoReferencia; ano >= this.getAno(); ano--){
                 
@@ -220,8 +112,6 @@ public class Cal {
         for(int r: this.getRm()){
             System.out.println(r);
         }
-        
-        
     }
     
     //Métodos especiais
@@ -232,15 +122,15 @@ public class Cal {
         this.atualizarS(2020, 2, 1);
     }
     
-    private void setMes(int m){
+    public void setMes(int m){
         this.mes = m;
     }
     
-    private int getMes(){
+    public int getMes(){
         return this.mes;
     }
     
-    private void setAno(int a){
+    public void setAno(int a){
         this.ano = a;
         
         // verificação de ano bissexto
@@ -248,7 +138,7 @@ public class Cal {
         this.getRm()[1] = fevereiro;
     }
     
-    private int getAno(){
+    public int getAno(){
         return this.ano;
     }
 
--- src/pkg03/calendario/Calendario.java
+++ src/pkg03/calendario/Calendario.java
@@ -5,6 +5,9 @@
  */
 package pkg03.calendario;
 
+import static java.lang.Thread.sleep;
+import java.util.Scanner;
+
 /**
  *
  * @author ander
@@ -15,9 +18,172 @@ public class Calendario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Cal calendario = new Cal();
-        calendario.exibirMes();
-        calendario.opcoes();
+        Cal cal = new Cal();
+        boolean continuar = true;
+        
+        do{
+            Calendario.exibirMes(cal);
+            continuar = Calendario.opcoes(cal);
+        }while(continuar);
     }
     
+    public static void exibirMes(Cal cal){
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        Calendario.interfaceCabecalhoMesAno(cal.getMes(), cal.getAno());
+        Calendario.interfaceCabecalhoSemana();
+        Calendario.interfaceCalendario(cal);
+    }
+        
+    // Métodos Auxiliares Nível 1
+    private static void interfaceCabecalhoMesAno(int m, int a){
+        String meses[] = {"JANEIRO", "FEVEREIRO", "MARÇO", "ABRIL", "MAIO", "JUNHO",
+                        "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"};        
+        int margem = (34 - (meses[m].length() + 8))/2;
+                
+        for(int contadorLinha = 0; contadorLinha <= 3; contadorLinha++){
+            for(int contadorColuna = 0; contadorColuna <= 35; contadorColuna++){
+                if((contadorLinha == 0 || contadorLinha == 3) && (contadorColuna != 0 && contadorColuna != 35)){
+                    System.out.print("_");
+                }else if(contadorLinha != 0 && (contadorColuna == 0 || contadorColuna == 35)){
+                    System.out.print("|");
+                }
+                else{
+                    if(contadorLinha == 2 && contadorColuna == margem + 1){
+                        System.out.print(meses[m] + " de " + a);
+                        contadorColuna += meses[m].length() + 7;
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.print("\n");
+        }       
+    }
+    
+    // Métodos Auxiliares Nível 1
+    private static void interfaceCabecalhoSemana(){
+        String semana[] = {"DO", "SE", "TE", "QA", "QI", "SE", "SA"};
+        int contadorColuna = 0;
+        int indice = 0;
+        
+        do{
+            if(contadorColuna % 5 == 2){
+                System.out.print(semana[indice]);
+                contadorColuna++;
+                indice++;
+            }else if(contadorColuna % 5 == 0){
+                System.out.print("|");
+            }else{
+                System.out.print(" ");
+            }
+            contadorColuna++; 
+        }while(contadorColuna <= 35);
+        System.out.println("");
+    }
+    
+    // Métodos Auxiliares Nível 1
+    private static void interfaceCalendario(Cal cal){
+        
+        int dia = 1;
+        int semanas_do_mes = Calendario.semanas_do_mes(cal);
+        boolean intervalo;
+        
+        
+        for(int contadorLinha = 0; contadorLinha <= semanas_do_mes * 3 ; contadorLinha++){
+            for(int contadorColuna = 0; contadorColuna <= 35; contadorColuna++){
+                if(contadorLinha % 3 == 0 && contadorColuna % 5 != 0){
+                    System.out.print("_");
+                }else if(contadorColuna % 5 == 0){
+                    System.out.print("|");
+                }else{
+                    intervalo = ((contadorLinha == 2 && contadorColuna >= (cal.getS() * 5) - 3)
+                                    || (contadorLinha > 2 && dia <= 28 + cal.getRm()[cal.getMes()]));
+                    if(intervalo && contadorLinha % 3 == 2 && contadorColuna % 5 == 2){
+                        String d = (dia < 10)? "0" + Integer.toString(dia): Integer.toString(dia);
+                        System.out.print(d);
+                        contadorColuna++;
+                        dia++;
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.print("\n");
+        }    
+    }
+    
+    //Método Auxiliar nível 2
+    private static int semanas_do_mes(Cal cal){
+        int semanas_mes = 5;
+        
+        if(cal.getS() == 1 && cal.getRm()[cal.getMes()] == 0){
+            semanas_mes = 4;
+        }else if((cal.getS() == 6 || cal.getS() == 7) && cal.getRm()[cal.getMes()] == 3){
+            semanas_mes = 6;
+        }else if(cal.getS() == 7 && cal.getRm()[cal.getMes()] == 2){
+            semanas_mes = 6;
+        }else{
+            semanas_mes = 5;
+        }
+        
+        return semanas_mes;
+    }
+    
+    //Método InterfaceGráfica
+    public static boolean opcoes(Cal cal){
+        Scanner leitor  = new Scanner(System.in);
+        String op;
+        
+        System.out.println("\n\n");
+        System.out.println("|--- LISTA DE AÇÕES ---|");
+        System.out.println("| a - Avançar um mês   |");
+        System.out.println("| v - Voltar um mês    |");
+        System.out.println("| p - Pesquisar um mês |");
+        System.out.println("| s - Sair             |");
+        System.out.println("|______________________|");
+        System.out.print("O que você deseja fazer?: ");
+        op = leitor.next();
+        
+        return Calendario.opcao(cal, op);
+    }
+    
+    // Métodos Auxiliares Nível 1
+    private static boolean opcao(Cal cal, String op){
+        boolean continuar = true;
+        Scanner leitor = new Scanner(System.in);
+        Thread tempo = new Thread();
+        
+        switch(op){
+            case "a":
+                cal.avancarMes();
+                break;
+            case "v":
+                cal.voltarMes();
+                break;
+            case "p":
+                System.out.println("--- Pesquisar Mês ---");
+                int ano, mes;
+                
+                System.out.print("ANO: ");
+                ano = leitor.nextInt();
+                System.out.print("MÊS: ");
+                mes = leitor.nextInt();
+                
+                if(mes < 1 || mes > 12){
+                    System.out.println("\n!!! MÊS INVÁLIDO !!!");
+                    System.out.print("Aperte qualquer tecla e enter para continuar ");
+                    leitor.next();
+                }else cal.pesquisarMes(mes - 1, ano);  
+                
+                break;
+            case "s":
+                continuar = false;
+                break;
+            default:
+                System.out.println("\n!!! OPÇÃO INVÁLIDA !!!");
+                System.out.print("Aperte qualquer tecla e enter para continuar ");
+                leitor.next();
+        }
+        return continuar;
+    }
 }
